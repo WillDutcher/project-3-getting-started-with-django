30de1cbeea51e62acbb2bc149df8473ad5eb8174
Will Dutcher <William.C.Dutcher@gmail.com>
2020-04-15 10:47:43 -0400

    First half of Chapter 18.

---
+++ learning_log/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for learning_log project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+
+------------------------------------------------------------------------------
+
+Helps Django serve the files it creates. (Web Server Gateway Interface = WSGI)
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_log.settings')
+
+application = get_wsgi_application()
---
+++ ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!g:\programming resources\books\python crash course\project-3-getting-started-with-django\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
